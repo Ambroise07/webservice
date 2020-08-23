67daed2de8ba67cdc8c945f34e09eacd3a3257f3
Ambroise <ambroise.houndonougbo@gmail.com>
2020-08-23 12:27:18 +0200

    status code update :: 200 && 400

--- cores/Helpers/Json.php
+++ cores/Helpers/Json.php
@@ -3,7 +3,13 @@ namespace App\Helpers;
 class Json{
     public static function message(bool $error = false,string $message=null, array $data = null){
         header('Content-Type: application/json');
-        return json_encode(['error' =>$error,'message'=>$message,'data'=>$data]);
+        if($error){
+            http_response_code(400);
+            return json_encode(['status' =>400,'message'=>$message,'data'=>$data]);
+        }else{
+            http_response_code(200);
+            return json_encode(['status' => 200,'message'=>$message,'data'=>$data]);
+        }
     }
 
 }
